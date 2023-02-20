90745095de0472e0857a1c41f2c1300a86613e73
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-02-21 03:03:59 +0530

    table items validated.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :expenses
   has_many :entities, through: :expenses
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
