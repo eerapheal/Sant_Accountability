8fc526e6a10e1794b4bee417f537125bc4fee6ec
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-02-21 19:03:48 +0530

    Home page controller generate and root set to home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  get 'home/index'
+  root to: 'home#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
